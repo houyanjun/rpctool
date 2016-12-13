e13421c5efb6d3192c553e9402119c3a13a02d22
houyanjun <w3887426@126.com>
2016-12-14 01:46:01 +0800

    change to maven and jdk8

---
+++ src/main/java/com/hou/server/HelloImpl.java
@@ -0,0 +1,21 @@
+package com.hou.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.caucho.hessian.server.HessianServlet;
+
+public class HelloImpl extends HessianServlet implements IHello {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String sayHello() {
+		List<String> list = new ArrayList<String>();
+		list.stream().filter(\)
+		return null;
+	}
+
+}
---
+++ src/main/java/com/hou/server/IHello.java
@@ -0,0 +1,5 @@
+package com.hou.server;
+
+public interface IHello {
+	String sayHello();
+}
